97e41f191254229f7725a9cf5e8cb261181d245a
Christopher Oye <christopheroye@gmail.com>
2019-05-31 23:03:21 +0000

    Initial commit

--- db/migrate/20190528220559_create_quotes.rb
+++
@@ -1,9 +0,0 @@
-class CreateQuotes < ActiveRecord::Migration[5.2]
-  def change
-    create_table :quotes do |t|
-      t.string :saying
-      t.string :author
-      t.timestamps
-    end
-  end
-end
