6f8fd844a4a84c169e1d370ec77c867b651b76ec
Christopher Oye <christopheroye@gmail.com>
2019-06-05 20:10:23 +0000

    updated Splurty file - application js folder with popper, bootstrap-sprockets, and tether

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require popper
+//= require tether
+//=require bootstrap-sprockets
 //= require_tree .
 //= require jquery3
 //= require popper
